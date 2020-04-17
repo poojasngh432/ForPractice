5d16a58eb9c123c09cb1092619af99c90e40cfa8
Pooja Singh <poojasngh432@gmail.com>
2020-04-18 02:00:30 +0530

    Revert "working"
    This reverts commit b200bc402fe486d6b2a085635d3d078a9b2941e8.

--- app/src/main/java/com/example/practiceproject/MainActivity.java
+++ app/src/main/java/com/example/practiceproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num = 2;
+        num = 1;
 
         //first change
     }
