6b7c63d98efcb10bcc24822731d1e35781849078
Pooja Singh <poojasngh432@gmail.com>
2020-04-18 01:54:07 +0530

    working on it

--- app/src/main/java/com/example/practiceproject/MainActivity.java
+++ app/src/main/java/com/example/practiceproject/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int num = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num = 1;
 
         //first change
     }
